15c6709ead4fc8653e301e4c2c9252fe25904705
MyroslavKucher <104145929+MyroslavKucher@users.noreply.github.com>
2022-06-24 20:20:32 +0300

    Delete JS directory

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-let menuBtn = document.querySelector('.header_burger'),
-    menu = document.querySelector('.menu');
-
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-})
